14dc3a876b659ec3181f82200c0f3cf3b9736ed8
haracky <ovymola@gmail.com>
2018-08-16 21:04:49 +0200

    character_input.py

--- character_input.py
+++ character_input.py
@@ -3,13 +3,17 @@
 ##################################################################
 # Program asks user for name and age
 # Returns the year in which you get 100 years
+import string
 
 name = "Dummy"
 age = 0
 
-name = str(input("Give me your name: "))
+name = input("Give me your name: ")
 age = int(input("Thank you!\nNow give me your age: "))
 
 result = 2018 - age + 100
 
-print(name + ", you will be 100 years old in " + str(result) +".")
+count = int(input("Good job!\nHow many times would you like to see result? "))
+
+for i in range(count):
+    print(name + ", you will be 100 years old in " + str(result) +".")
